bd64ca316b8cdddbb0a2009edc8d43f0d16e1106
Ido <idogada@agatsoftware.com>
2021-01-12 15:46:04 +0200

    Update to module version

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from  SubModule import submodule
 import json
-version = '1.0.5'
+version = '1.0.6'
 app = Flask(__name__)
 app.register_blueprint(submodule, url_prefix='/submodule')
 
