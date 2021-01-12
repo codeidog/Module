6e5ed63c3a2cb26a36a03f23756e8c41a54d431f
Ido <idogada@agatsoftware.com>
2021-01-12 08:51:11 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from  SubModule import submodule
+import json
+version = 1.0
+app = Flask(__name__)
+app.register_blueprint(submodule, url_prefix='/submodule')
+
+@app.route('/')
+def index():
+    global version
+    returnDict = {
+        'name' : __name__,
+        'version': version
+    }
+    return json.dumps(returnDict, indent=4, sort_keys=True)
+
+if __name__ == "__main__":
+
+    app.run()
