8c80204c55643e224974f75b58255a12ad2f6898
Ido <ido@pop-os.localdomain>
2021-01-13 10:31:47 +0200

    Update to Module

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from  SubModule import submodule
 import json
 import logging
-version = '1.0.11'
+version = '1.0.12'
 app = Flask(__name__)
 app.register_blueprint(submodule, url_prefix='/submodule')
 
