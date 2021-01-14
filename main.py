29e8aea2480ff03a8d62ad12520ef6395bd473ad
idogada9087 <72713118+idogada9087@users.noreply.github.com>
2021-01-14 19:54:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from  SubModule import submodule
 import json
 import logging
-version = '1.0.1.7'
+version = '1.0.1.8'
 
 app = Flask(__name__)
 app.register_blueprint(submodule, url_prefix='/submodule')
